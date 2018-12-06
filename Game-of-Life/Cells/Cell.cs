d46f4e0eda09ac026ff573e00a948291c5fc19ae
Zet <mortgage.entertainment@gmail.com>
2018-12-06 20:25:22 +0300

    Add Cell class file

---
+++ Game-of-Life/Cells/Cell.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Game_of_Life
+{
+    class Cell
+    {
+        /*Класс обычной клетки. 
+         * Он объединяет общие черты у разных 
+         * типов клеток для передачи через 
+         * наследование.*/
+
+        private byte saturation;  // переменная насыщения, которая хранит степень насыщения определённой клетки в процентах
+    }
+}
