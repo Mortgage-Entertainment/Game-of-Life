336cdd9c2cc4e819953d89377ff6e545b4f05266
devDI <Developer.Dalani.Ioan@yandex.ru>
2019-02-06 07:03:03 +0300

    Добавлен обработчик FPS и его измеритель

---
+++ Game-of-Life/Options/Handlers.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Threading.Timer;
+
+namespace Game_of_Life.Options
+{
+    class Handlers
+    {
+        /*
+         *  Класс обработчиков событий
+         *  
+         *  Здесь пишутся все обработчики
+         *   например, один из важнейших в игре, обработчик фпс
+         *   
+         */
+         
+        //---------------------------------------------------Обработчик FPS------------------------------------------------------------------------\\
+        
+        int FramesCount = 0;
+        int FPSCount;
+
+        private void OnIdle()
+        {
+            /*
+             *  Обработчик события простоя приложения
+             *  
+             *  Выступает в роли таймера 
+             *   с интелектуальным интервалом
+             *    ( как только доступен новый цикл (кадр), один будет выполнен )
+             *  
+             *  Оброботчик FPS
+             *  
+             */
+            
+
+            sleep(100);   //  ограничение ( чтобы на начальных этапах разработки фпс не имел недопустимых значений )
+
+            FramesCount++;
+        }
+
+        private void FPSMeterM()
+        {
+            /*
+             *  Таймер с интервалом в секунду
+             *  
+             *  Нужен для измерения фпс
+             *   ( кол-ва проходов OnIdle 
+             *     за один проход этого таймера )
+             *  
+             */
+            
+
+            FPSCount = FramesCount;
+            FramesCount = 0;
+        }
+
+        public void FPSInitialize()
+        {
+            /*
+             *  Инициализирует 
+             *    обработчик FPS и
+             *    его измеритель
+             *  
+             *  Создаёт таймер и назначет его обработчик
+             *  
+             *  Назначает OnIdle ( обработчик FPS )
+             *  
+             */
+            
+
+            TimerCallback fpsm = new TimerCallback(FPSMeterM);
+            Timer FPSMeterT = new Timer(fpsm, 0, 1000);
+
+            Application.Idle = new EventHandler(OnIdle);
+        }
+    }
+}
